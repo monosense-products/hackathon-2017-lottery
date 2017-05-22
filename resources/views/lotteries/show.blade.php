2c2cdef3814accb529e0b7c3e5064ee6413773b8
モノセンス八木 <yagi@monosense.co.jp>
2017-05-22 12:05:24 +0900

    数量のMAXを5に変更

--- resources/views/lotteries/show.blade.php
+++ resources/views/lotteries/show.blade.php
@@ -50,11 +50,6 @@
                         <option value="3">3</option>
                         <option value="4">4</option>
                         <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
                     </select>
                     <button type="submit" class="btn btn-primary">くじを購入する</button>
                 </div>
